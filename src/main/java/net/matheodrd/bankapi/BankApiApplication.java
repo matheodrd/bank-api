a08698d320c02f7ee7b17ac5413d32dec1bc3b6a
Mathéo Durand <matheodurand14@gmail.com>
2025-11-05 14:43:27 +0100

    feat: init Spring Boot project

---
+++ src/main/java/net/matheodrd/bankapi/BankApiApplication.java
@@ -0,0 +1,13 @@
+package net.matheodrd.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BankApiApplication.class, args);
+    }
+
+}
