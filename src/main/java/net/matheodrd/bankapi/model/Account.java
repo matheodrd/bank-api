75616e3988ce4f07180674bfb8f208094534025e
Mathéo Durand <matheodurand14@gmail.com>
2025-11-05 21:37:31 +0100

    feat: add Account entity

---
+++ src/main/java/net/matheodrd/bankapi/model/Account.java
@@ -0,0 +1,46 @@
+package net.matheodrd.bankapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import net.matheodrd.bankapi.model.enums.AccountStatus;
+import net.matheodrd.bankapi.model.enums.Currency;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "account_holder")
+    private String accountHolder;
+
+    @Column(name = "balance")
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency")
+    private Currency currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private AccountStatus status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
