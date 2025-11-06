2d5a0cb561a6e0a0c5ddfe9707ccc8921b807a22
Mathéo Durand <matheodurand14@gmail.com>
2025-11-06 01:00:32 +0100

    feat: add CreateAccountRequest DTO

---
+++ src/main/java/net/matheodrd/bankapi/dto/request/CreateAccountRequest.java
@@ -0,0 +1,23 @@
+package net.matheodrd.bankapi.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import net.matheodrd.bankapi.model.enums.Currency;
+
+import java.math.BigDecimal;
+
+public record CreateAccountRequest(
+        @NotBlank(message = "Account holder is required")
+        @Size(min = 2, max = 100)
+        String accountHolder,
+
+        @NotNull(message = "Initial balance is required")
+        @DecimalMin(value = "0.0")
+        BigDecimal initialBalance,
+
+        @NotBlank
+        Currency currency
+) {
+}
