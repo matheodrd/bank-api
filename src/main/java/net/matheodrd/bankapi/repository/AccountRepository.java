695fb4b2c6146ab21a9d8436c51f830378d18a73
Mathéo Durand <matheodurand14@gmail.com>
2025-11-06 20:18:58 +0100

    fix: rename JPA query method in AccountRepository

--- src/main/java/net/matheodrd/bankapi/repository/AccountRepository.java
+++ src/main/java/net/matheodrd/bankapi/repository/AccountRepository.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
-    boolean existsAccountByAccountNumber(String accountNumber);
+    boolean existsByAccountNumber(String accountNumber);
 
     Optional<Account> findByAccountNumber(String accountNumber);
 
